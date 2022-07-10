a9a25e64109b887c72ede7115fd107f3fcfb1738
simon <simon.popelier@gmail.com>
2022-07-10 23:45:07 +0200

    Tensor size correction

--- link_predictor.py
+++ link_predictor.py
@@ -11,6 +11,7 @@ class LinkPredictor(pl.LightningModule):
         self.conv1 = GCNConv(in_channels, hidden_channels)
         self.conv2 = GCNConv(hidden_channels, out_channels)
         self.edge_index = edge_index
+        self.feature_nb = in_channels
 
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
@@ -22,7 +23,10 @@ class LinkPredictor(pl.LightningModule):
         return self.conv2(x, edge_index)
 
     def decode(self, z, edge_label_index):
-        return (z[edge_label_index[0]] * z[edge_label_index[1]]).sum(dim=-1)
+        index_1 = edge_label_index[0]
+        index_2 = edge_label_index[1]
+        q = z[index_1] * z[index_2]
+        return q.sum(dim=-1)
 
     def decode_all(self, z):
         prob_adj = z @ z.t()
@@ -30,22 +34,21 @@ class LinkPredictor(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         y_hat, y = self._step(batch)
-        loss =F.binary_cross_entropy_with_logits(y_hat, y)
+        loss =F.binary_cross_entropy_with_logits(y_hat, y.float())
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         y_hat, y = self._step(batch)
-        loss =F.binary_cross_entropy_with_logits(y_hat, y)
+        loss =F.binary_cross_entropy_with_logits(y_hat, y.float())
         self.log("val_loss", loss)
         return loss
 
     def _step(self, batch):
         x, selected_edge_indexes, y = batch
-        x = x.view(x.size(0), -1)
         z = self.encode(x, self.edge_index)
-        y_hat = self.decode(z, selected_edge_indexes)
-        return y_hat, y
+        y_hat = self.decode(z.view(x.size(1), -1), selected_edge_indexes.view(2, -1))
+        return y_hat, y.squeeze()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
