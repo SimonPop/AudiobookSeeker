cce52e6de4690a80a8d76ffd837c89d229ee80fc
simon <simon.popelier@gmail.com>
2022-07-14 16:06:06 +0200

    Audible book class for clarity - refacto scrapper to adapt to it.

---
+++ audible_book.py
@@ -0,0 +1,165 @@
+import numpy as np
+import re
+
+from torch import le
+
+
+class AudibleBook:
+    def __init__(
+        self,
+        id=None,
+        title=None,
+        subtitle=None,
+        author=None,
+        narrator=None,
+        stars=None,
+        length=None,
+        ratings=None,
+        links=None,
+        recommendation_ids=None,
+    ):
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.author = author
+        self.narrator = narrator
+        self.length = length
+        self.stars = stars
+        self.ratings = ratings
+        self.links = links
+        self.recommendation_ids = recommendation_ids
+
+    def __str__(self) -> str:
+        return """
+        Audible Book : {} - {}
+        ID: {}
+        Author: {}
+        Narrator: {}
+        Stars: {}
+        Ratings: {}
+        Length: {}
+        """.format(
+            self.title,
+            self.subtitle,
+            self.id,
+            self.author,
+            self.narrator,
+            self.stars,
+            self.ratings,
+            self.length,
+        )
+
+    def create_book_from_request(self, r, url):
+        self.id = url.split("/")[5].split("?")[0]
+        information_dict = self.parse_book(r)
+        self.title = information_dict["title"]
+        self.subtitle = information_dict["subtitle"]
+        self.author = information_dict["author"]
+        self.narrator = information_dict["narrator"]
+        self.length = information_dict["length"]
+        self.stars = information_dict["stars"]
+        self.links = information_dict["links"]
+        self.ratings = information_dict["ratings"]
+        self.recommendation_ids = information_dict["recommendations"]
+        return self
+
+    def parse_book(self, r):
+        """Parse book information from given raw html result."""
+        # Links / Recommendations.
+        recommendation_ids, recommendation_links = AudibleBook._parse_recommendations(r)
+
+        # Book information
+        information_dict = AudibleBook._parse_book_info(r)
+
+        information_dict["id"] = id
+        information_dict["recommendations"] = recommendation_ids
+        information_dict["links"] = recommendation_links
+
+        # Raw -> Selected
+        (
+            information_dict["stars"],
+            information_dict["ratings"],
+        ) = AudibleBook.extract_stars_rating(information_dict["stars"])
+        information_dict["length"] = AudibleBook.extract_time(
+            information_dict["length"]
+        )
+
+        return information_dict
+
+    @staticmethod
+    def _parse_book_info(r):
+        title = (
+            r.html.find(
+                ".bc-list.bc-list-nostyle.bc-color-secondary.bc-spacing-s2", first=True
+            )
+            .find("h1", first=True)
+            .text
+        )
+
+        subtitle = (
+            r.html.find(
+                ".bc-list.bc-list-nostyle.bc-color-secondary.bc-spacing-s2", first=True
+            )
+            .find(".bc-text.bc-size-medium", first=True)
+            .text
+        )
+
+        author = (
+            r.html.find(".bc-list-item.authorLabel", first=True)
+            .find("a", first=True)
+            .text
+        )
+        narrator = (
+            r.html.find(".bc-list-item.narratorLabel", first=True)
+            .find("a", first=True)
+            .text
+        )
+        length = r.html.find(".bc-list-item.runtimeLabel", first=True).text
+        ratings = r.html.find(".bc-list-item.ratingsLabel", first=True).text
+
+        return {
+            "title": title,
+            "subtitle": subtitle,
+            "author": author,
+            "narrator": narrator,
+            "length": length,
+            "stars": ratings,
+        }
+
+    @staticmethod
+    def extract_time(x):
+        time_pattern = re.compile("Length: ([0-9]*) hrs and ([0-9]*) mins")
+        time_res = time_pattern.search(x)
+        if time_res is None:
+            return np.NaN, np.NaN
+        else:
+            return float(time_res.group(1)[0]), float(time_res.group(2)[0])
+
+    @staticmethod
+    def extract_stars_rating(raw_stars):
+        ratings = stars = np.NaN
+        if raw_stars is not None and str(raw_stars) != "nan":
+            stars = float(
+                re.search(r"(^[0-9,.]+) ", raw_stars).group(1).replace(",", ".")
+            )
+            v1 = re.search(r" ([0-9,]+)$", raw_stars)
+            v2 = re.search(r"\(([0-9,]*) ratings\)$", raw_stars)
+            ratings = float(
+                v1.group(1).replace(",", ".")
+                if v1 is not None
+                else v2.group(1).replace(",", ".")
+            )
+        return stars, ratings
+
+    @staticmethod
+    def _parse_recommendations(r):
+        recommendations = r.html.find(".carousel-product")
+        recommendation_ids = []
+        recommendation_links = []
+        for reco in recommendations:
+            reco_links = [s for s in reco.links if s.split("/")[1] == "pd"]
+            recommendation_links.extend(reco_links)
+            recommendation_ids.extend(
+                [s.split("/")[3].split("?")[0] for s in reco_links]
+            )
+        return recommendation_ids, recommendation_links
--- scrapper.py
+++ scrapper.py
@@ -1,54 +1,53 @@
 from requests_html import HTMLSession, HTML
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import warnings
-from os.path import join
+from os.path import join, exists
 from collections import deque
 from time import sleep
+from audible_book import AudibleBook
+from data_manager import DataManager
 
 warnings.filterwarnings("ignore")
 
-class AudibleScrapper():
 
-    def __init__(self):
+class AudibleScrapper:
+    def __init__(self, df_path="audible_raw_data.csv"):
         self.base_audible = "https://www.audible.com"
-        self.categories_url = 'https://www.audible.com/categories?ref=a_pd_Americ_t1_navTop_pl0cg0c0r5&pf_rd_p=2774f9d8-d3de-47f2-8c6a-6a59524b5923&pf_rd_r=ZS5GW1KHJ8NGV7PSA3N3'
+        self.categories_url = "https://www.audible.com/categories?ref=a_pd_Americ_t1_navTop_pl0cg0c0r5&pf_rd_p=2774f9d8-d3de-47f2-8c6a-6a59524b5923&pf_rd_r=ZS5GW1KHJ8NGV7PSA3N3"
         self.sess = HTMLSession()
 
+        self.data_manager = DataManager()
 
     def get_all_categories(self, base_url):
         """Gets links for each category."""
         r = self.sess.get(base_url)
 
-        genres = r.html.find('div.bc-col-responsive.bc-col-3')
+        genres = r.html.find("div.bc-col-responsive.bc-col-3")
         cat_items = []
         cat_names = []
 
         for genre in genres:
-            items = genre.find('ul.bc-list')[0].find('li.bc-list-item')
+            items = genre.find("ul.bc-list")[0].find("li.bc-list-item")
             cat_items.extend(items)
 
-        cat_names = [item.text.split(' (')[0] for item in cat_items]
+        cat_names = [item.text.split(" (")[0] for item in cat_items]
 
         return cat_items, cat_names
 
-
     def get_page_list(self, category_link):
         """Gets the link for a page of books a given category."""
-        
+
         r = self.sess.get(category_link)
-        all_link = r.html.find('.allInCategoryPageLink', first=True).attrs['href']
+        all_link = r.html.find(".allInCategoryPageLink", first=True).attrs["href"]
 
         return all_link
 
     def crawl_category_page(self, category_all_link, page=0):
         """Get the links of all books in a page of a given category."""
-        
-        r = self.sess.get(category_all_link) #TODO: add page number
-        book_links = r.html.find('li.bc-list-item.productListItem', first=False)
 
-        return book_links 
+        r = self.sess.get(category_all_link)  # TODO: add page number
+        book_links = r.html.find("li.bc-list-item.productListItem", first=False)
+
+        return book_links
 
     def parse_page(self, page_link):
         """Parse an audible book's page."""
@@ -56,64 +55,29 @@ class AudibleScrapper():
         self.sess = HTMLSession()
 
         r = self.sess.get(page_link, allow_redirects=False)
-        # Own ID
-        id = page_link.split('/')[5].split('?')[0]
 
-        # Recommendations
-        if r.text == "":
-            return {'links': []}
-
-        recommendations = r.html.find('.carousel-product')
-        recommendation_ids = []
-        recommendation_links = []
-        for reco in recommendations:
-            reco_links = [s for s in reco.links if s.split('/')[1] == "pd"]
-            recommendation_links.extend([self.base_audible + r for r in reco_links])
-            recommendation_ids.extend([s.split('/')[3].split('?')[0] for s in reco_links])
-
-        # Local information
-        local_fields = ['title', 'subtitle', 'author', 'narrator', 'length', 'stars']
-        local_info = r.html.find('.bc-list.bc-list-nostyle.bc-color-secondary.bc-spacing-s2')[0].text.split('\n')
-        
-        information_dict = dict(zip(local_fields,local_info))
-        information_dict['id'] = id
-        information_dict['recommendations'] = recommendation_ids
-        information_dict['links'] = recommendation_links
-
-        return information_dict 
+        return AudibleBook().create_book_from_request(r, page_link)
 
     def close_session(self):
         self.sess.close()
 
-    def random_walk(self, link = None, limit = 10, results = {}, sleep_time = 1):
-    
-        if link is not None:
-            information_dict = self.parse_page(link)
-            id = link.split('/')[5].split('?')[0]
-            results[id] = information_dict
+    def random_walk(self, starting_link, limit=10, results={}, sleep_time=1):
+        next_links = deque([starting_link])
+        results = {}
 
-        next_links = deque()
-        for v in results.values():
-            next_links.extend(v['links'])
+        while len(next_links) > 0 and len(results) < limit:
 
-        iteration = 0
-        while len(next_links) > 0 and iteration < limit:
             link = next_links.pop()
-            id = link.split('/')[5].split('?')[0]
 
-            if (id not in results):
+            id = link.split("/")[5].split("?")[0]
+
+            if id not in results:
                 try:
-                    information_dict = self.parse_page(link)
-                    results[id] = information_dict
-                    print('Processed - {}'.format(id))
-                    next_links.extend(information_dict['links'])
-                    print(next_links)
-                    iteration += 1
-                    sleep(sleep_time)
+                    book = self.parse_page(link)
+                    results[id] = book
+                    next_links.extend([self.base_audible + l for l in book.links])
                 except:
-                    print('Impossible to parse page {}'.format(id))
-            else:
-                print(id)
-                print(next_links)
-                
+                    print("Impossible to parse page {}".format(link))
+                    results[id] = None
+
         return results
